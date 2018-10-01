499364f9de71a5bce560f0b2a40784111a7a5885
Jesus Salcedo <sjsalcedoa@unal.edu.co>
2018-09-30 22:35:47 -0500

    Cambios en seed.rb

--- Application/db/seeds.rb
+++ Application/db/seeds.rb
@@ -8,47 +8,49 @@
 
 # poblar base de datos usando gem FAKER
 100.times do
-  Answer.create(
-      answerable_type: Answer,
-      answerable_id:Faker::Number.number(5),
-      idAnswer:Faker::Number.number(5),
-      Description: Faker::String.random(24),
-      Qualification:Faker::Number.number(2),
-      Date:Faker::Date.forward(10)
-  )
+  Answer.create([{
+    answerable_type: Answer,
+    answerable_id:Faker::Number.number(5),
+    idAnswer:Faker::Number.number(5),
+    Description: Faker::String.random(24),
+    Qualification:Faker::Number.number(2),
+    Date:Faker::Date.forward(10)
+  }])
 end
 
 
 100.times do
-  Comment.create(
+  Comment.create([{
+      commentable_type: Comment,
+      commentable_id:Faker::Number.number(5),
       idComment:Faker::Number.number(5),
       Description:Faker::String.random(24),
       Date: Faker::Date.forward(10)
-  )
+  }])
 end
 
 100.times do
-  Question.create(
+  Question.create([{
       questionable_type: Question,
       questionable_id: Faker::Number.number(5),
       idQuestion: Faker::Number.number(5),
       Title:Faker::String.random(20),
       Description:Faker::String.random(24),
       Date:Faker::Date.forward(10)
-  )
+  }])
 end
 
 100.times do
-  Theme.create(
+  Theme.create([{
       idTheme:Faker::Number.number(5),
       Name:Faker::Number.number(7),
       Questions_number:Faker::Number.number(3),
       idMatter:Faker::Number.number(5)
-  )
+  }])
 end
 
 100.times do
-  User.create(
+  User.create([{
       idUser: Faker::Number.number(5),
       Name: Faker::Name.name_with_middle,
       Level: Faker::Number.number(2),
@@ -56,7 +58,7 @@ end
       Role: "user",
       Followers_number: Faker::Number.number(3),
       idStat: Faker::Number.number(5)
-  )
+  }])
 end
 
 
