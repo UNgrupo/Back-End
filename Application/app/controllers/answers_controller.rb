2559008fd23e0996e2bfe75e988b7542f3d60bc3
Jesus Salcedo <sjsalcedoa@unal.edu.co>
2018-09-30 20:16:47 -0500

    Rutas & Controladores

---
+++ Application/app/controllers/answers_controller.rb
@@ -0,0 +1,50 @@
+class AnswersController < ApplicationController
+
+    # para GET
+    def index
+      answers = Answer.all
+      render json:answers, status:200
+    end
+
+    # para SHOW
+    def show
+      answer = Answer.find(params[:idAnswer])
+      respond_to do |format|
+         format.json {render json: answer, status:200}
+      end
+    end
+
+    # Para POST
+    def create
+        answer = Answer.new(params_user)
+        if user.save
+            render json:answer, status:201
+        else
+            render json:answer.errors, status: :unprocessable_entity
+        end
+    end
+
+    # para DELETE
+    def destroy
+        answer = Answer.find(params[:idAnswer])
+        answer.destroy
+        respond_to do |format|
+            format.json {render json: answer, status: 200}
+        end
+    end
+
+    #para PUT o PATCH
+    def update
+        answer = Answer.find(params[:id])
+        if answer.update(params_answer)
+            render json: answer, status: 200
+        else
+            render json: answer.errors, status: 422
+        end
+
+    end
+
+    def params_answer
+        params.permit(:idAnswer)
+    end
+end
---
+++ Application/app/controllers/themes_controller.rb
@@ -0,0 +1,49 @@
+class ThemesController < ApplicationController
+  # para GET
+  def index
+    themes = Theme.all
+    render json:themes, status:200
+  end
+
+  # para SHOW
+  def show
+    theme = Theme.find(params[:idTheme])
+    respond_to do |format|
+       format.json {render json:theme , status:200}
+    end
+  end
+
+  # Para POST
+  def create
+      theme = Theme.new(params_theme)
+      if theme.save
+          render json:theme, status:201
+      else
+          render json:theme.errors, status: :unprocessable_entity
+      end
+  end
+
+  # para DELETE
+  def destroy
+      theme = Theme.find(params[:idTheme])
+      theme.destroy
+      respond_to do |format|
+          format.json {render json: theme, status: 200}
+      end
+  end
+
+  #para PUT o PATCH
+  def update
+      theme = Theme.find(params[:idTheme])
+      if theme.update(params_theme)
+          render json: theme, status: 200
+      else
+          render json: theme.errors, status: 422
+      end
+
+  end
+
+  def params_answer
+      params.permit(:idTheme, :Name)
+  end
+end
