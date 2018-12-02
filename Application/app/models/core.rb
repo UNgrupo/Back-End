cbf16f1f719d8ff38899e1838f49b1acc4ef1ea6
Jesus Salcedo <sjsalcedoa@unal.edu.co>
2018-12-02 18:08:35 -0500

    Cambio del Api consumida

--- Application/app/models/core.rb
+++ Application/app/models/core.rb
@@ -1,11 +1,14 @@
 class Core < ApplicationRecord
 
     include HTTParty
-    base_uri 'http://www.clashapi.xyz'
+    #base_uri 'http://www.clashapi.xyz'
+    base_uri 'https://api.apis.guru'
     format :json
 
     def self.for(term)
-        get("/api/#{term}")
+        #get("/api/#{term}")
+        get("/v2/#{term}")
+
     end
 
 end
